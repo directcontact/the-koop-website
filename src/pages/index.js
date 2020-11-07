e5d3427ae30e7d5174634161e1aba4556e43ddf0
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2020-11-07 18:11:17 -0500

    finishing basic outline for main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,15 @@ export default class IndexPage extends React.Component {
   }
 
   render() {
-    return <Layout>Hello</Layout>;
+    return (
+      <Layout>
+        <div className="main">
+          <h1 className="main__header">THE KOOP</h1>
+          <h2 className="main__subheader">
+            KOREAN CHICKEN | CUISINE | JOCKBAL
+          </h2>
+        </div>
+      </Layout>
+    );
   }
 }
