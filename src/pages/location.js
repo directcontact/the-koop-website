5c0d31e5df3e1bcf96c59e7334bddba285134116
estherkang14 <estherkang14@gmail.com>
2020-12-13 00:05:45 -0500

    returned location to og. going to attempt leaflet again lol

--- src/pages/location.js
+++ src/pages/location.js
@@ -1,3 +1,5 @@
+
+
 export default class LocationPage extends React.Component {
   constructor() {
     super();
@@ -17,8 +19,22 @@ export default class LocationPage extends React.Component {
                 <strong>Fri - Sat</strong>: 11am - 10pm
               </div>
           </div>
-            <div className="location__block-first_map col-md-6"> 
-              map goes here
+            <div className="location__block-first_map col-md-6"> MAP GOES HERE
+              {/* <div id="mapid">
+                <MapContainer center={[40.79629915489803, -77.85769951576074]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[40.79629915489803, -77.85769951576074]}>
+                      <Popup>
+                        The Koop @ State College <br />
+                        129 Locust Ln, State College, PA 16801<br />
+                        814-954-7807
+                      </Popup>
+                    </Marker>
+                </MapContainer>
+              </div> */}
             </div>
         </div>
       </>
@@ -29,8 +45,22 @@ export default class LocationPage extends React.Component {
     return (
       <>
         <div className="location__block col-md-12">
-          <div className="location__block-second_map col-md-6">
-            map goes here
+          <div className="location__block-second_map col-md-6"> MAP GOES HERE
+            {/* <div classname="location__map">
+              <MapContainer center={[40.239090007995536, -76.94114703111805]} zoom={13} scrollWheelZoom={false}>
+                  <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={[40.239090007995536, -76.94114703111805]}>
+                    <Popup>
+                      The Koop @ Camp Hill <br />
+                      5 South 35th Street, Camp Hill, PA 17011<br />
+                      717-695-7930
+                    </Popup>
+                  </Marker>
+              </MapContainer>
+            </div> */}
           </div>  
             
           <div className="location__block-second col-md-6">
