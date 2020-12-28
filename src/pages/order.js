f6998c590a95928b3e18acf1c81273fe158ce210
estherkang14 <estherkang14@gmail.com>
2020-12-28 17:43:01 -0500

    Build order page - background image + incorporating state

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import Cart from '../components/cart'
 
 export default class OrderingPage extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      navActive: {
+        step: 'location',
+        selection: '',
+        cart: [],
+        active: 'selected',
+      },
+      menuItems: [
+        'CHICKEN',
+        'APPETIZERS',
+        'RICE DISHES',
+        'TROTTER',
+        'SOUPS',
+        'SIDES',
+      ],
+    };
   }
 
-  
+  renderLocationSelection(navActive) {
+      // location selection screen
+      // choose between state college or camp hill (title on left, address + phone num on right)
+      // unselected is cream/light tan color. selected is mocha brown color
+  }
+
+  renderOrderComponent(navActive) {
+    switch (navActive.selection) {
+      case 'chicken':
+      case 'appetizers':
+      case 'rice dishes':
+      case 'trotter':
+      case 'soups':
+      case 'sides':
+      default:
+        return null;
+    }
+  }
 
   render() {
     return (
